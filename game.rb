6b5b8b98ce361f06b8db02ecf7c71a6587c09758
root <root@LAPTOP-AQR2PGDF.localdomain>
2020-10-10 11:45:08 +0530

    added choicemap to finished

--- game.rb
+++ game.rb
@@ -624,6 +624,16 @@ class Finished < Scene
   def enter()
     system ('clear')
     puts "More coming soon!"
+    sleep 1.5
+    puts"/n"
+    puts"/n"
+    puts "There are many countless endings to your tale, Brave Hero! Try again to see what story unfolds!" 
+    sleep 1.5
+    puts "\n"
+    puts "\n"
+    require("./assets/asciiart.rb")
+    Asciiart.render("./assets/choicemap.txt")
+    sleep 1.5
     exit(1)
   end
 end
